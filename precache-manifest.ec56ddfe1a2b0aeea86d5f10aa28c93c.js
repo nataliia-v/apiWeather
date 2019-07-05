f347d8e26e335adaf6ff6c0eedd0d266f166df14
nataliia-v <nataly.verbenskaya@gmail.com>
2019-07-05 22:08:16 +0300

    Updates

---
+++ precache-manifest.ec56ddfe1a2b0aeea86d5f10aa28c93c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8d3651073d033f1be847ad1db7d18f4",
+    "url": "/apiWeather/static/media/bg-main.b8d36510.jpg"
+  },
+  {
+    "revision": "7e9559b3693d4ed5902a",
+    "url": "/apiWeather/static/js/runtime~main.757034f8.js"
+  },
+  {
+    "revision": "78d124f22df2fd799337",
+    "url": "/apiWeather/static/js/main.c9361d4c.chunk.js"
+  },
+  {
+    "revision": "0cf2a1f644c0fa17962a",
+    "url": "/apiWeather/static/js/2.7d04f3d4.chunk.js"
+  },
+  {
+    "revision": "78d124f22df2fd799337",
+    "url": "/apiWeather/static/css/main.ecf0dd27.chunk.css"
+  },
+  {
+    "revision": "0cf2a1f644c0fa17962a",
+    "url": "/apiWeather/static/css/2.f14583aa.chunk.css"
+  },
+  {
+    "revision": "17e45d9b34eb5a0ff2e1ccea2a38f462",
+    "url": "/apiWeather/index.html"
+  }
+];
